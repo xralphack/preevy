34793c218029a8ce16b39232abaee94deded3c72
ralphchen <xralphack@gmail.com>
2025-04-29 14:55:20 +0800

    feat: open 27017 port

--- packages/driver-lightsail/src/driver/client.ts
+++ packages/driver-lightsail/src/driver/client.ts
@@ -206,6 +206,13 @@ const client = ({
             cidrs: ['0.0.0.0/0'],
             ipv6Cidrs: ['::/0'],
           },
+          {
+            fromPort: 27017,
+            toPort: 27017,
+            protocol: 'tcp',
+            cidrs: ['0.0.0.0/0'],
+            ipv6Cidrs: ['::/0'],
+          },
         ],
       })
 
